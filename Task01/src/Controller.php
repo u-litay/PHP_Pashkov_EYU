4a1e742b79354db63246006c459de20da77d863f
u-litay <grishaevzzz@mail.ru>
2025-02-25 12:47:24 +0300

    Выполнил первое задание

---
+++ Task01/src/Controller.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Task01\src\Controller;
+
+use Task01\src\View;
+
+function startGame()
+{
+
+    View\displayStartScreen();
+}
---
+++ Task01/src/View.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Task01\src\View;
+
+function displayStartScreen()
+{
+
+    echo "Добро пожаловать в игру.\n";
+}
